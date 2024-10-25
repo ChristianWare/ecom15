ae770a0415263629d0176e07c2d5f4f4151d1dab
Chris Ware <c.p.ware@hotmail.com>
2024-10-24 21:33:20 -0700

    Created Product component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,59 @@
+import Product from "@/components/Product";
+import { delay } from "@/lib/utils";
+import { getWixClient } from "@/lib/wix-client.base";
+import { Suspense } from "react";
 
-export default function Home() {
+export default async function Home() {
   return (
-    <main>
-      <h1>yo</h1>
+    <main className="mx-auto max-w-7xl space-y-10 px-5 py-10">
+      <div className="bg-secondary flex items-center md:h-96">
+        <h1>Hero stuff here</h1>
+      </div>{" "}
+      <Suspense fallback="Loading...">
+        <FeaturedProducts />
+      </Suspense>
     </main>
   );
+}
+
+async function FeaturedProducts() {
+  await delay(1000);
+
+  const wixClient = getWixClient();
+
+  const { collection } =
+    await wixClient.collections.getCollectionBySlug("featured-products");
+
+  if (!collection?._id) {
+    return null;
+  }
+
+  const featuredProducts = await wixClient.products
+    .queryProducts()
+    .hasSome("collectionIds", [collection._id])
+    .descending("lastUpdated")
+    .find();
+
+  if (!featuredProducts.items.length) {
+    return null;
+  }
+
+  return (
+    <div className="spacey5">
+      <h2 className="text-2xl font-bold">Featured Products</h2>
+      <div className="flex grid-cols-2 flex-col sm:grid md:grid-cols-3 lg:grid-cols-4">
+        {featuredProducts.items.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function loadingSkeleton() {
+  <div className="flex grid-cols-2 flex-col pt-12 sm:grid md:grid-cols-3 lg:grid-cols-4">
+    {Array.from({ length: 8 }).map((_, i) => (
+      <div key={i}>Skeloton here</div>
+    ))}
+  </div>;
 }
