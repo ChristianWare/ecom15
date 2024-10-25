28d095311859ea00bb6a92917e4a8dc4de369bd9
Chris Ware <c.p.ware@hotmail.com>
2024-10-25 16:32:33 -0700

    Product options start

--- src/app/products/[slug]/ProductDetails.tsx
+++ src/app/products/[slug]/ProductDetails.tsx
@@ -2,12 +2,26 @@
 
 import WixImage from "@/components/WixImage";
 import { products } from "@wix/stores";
+import ProductOptions from "./ProductOptions";
+import { useState } from "react";
 
 interface ProductDetailsProps {
   product: products.Product;
 }
 
 export default function ProductDetails({ product }: ProductDetailsProps) {
+  const [quantity, setQunatity] = useState(1);
+
+  const [selectedOptions, setSelectedOptions] = useState<
+    Record<string, string>
+  >(
+    product.productOptions
+      ?.map((option) => ({
+        [option.name || ""]: option.choices?.[0].description || "",
+      }))
+      ?.reduce((acc, curr) => ({ ...acc, ...curr }), {}) || {},
+  );
+
   return (
     <div className="flex flex-col gap-10 md:flex-row lg:gap-20">
       <div className="basis-2/5">
@@ -22,6 +36,19 @@ export default function ProductDetails({ product }: ProductDetailsProps) {
       <div className="basis-3/5 space-y-5">
         <div className="space-y-2.5">
           <h1 className="text-3xl font-bold lg:text-4xl">{product.name}</h1>
+          {product.brand && <p>Brand: {product.brand}</p>}
+          {product.ribbon && <p>Ribbon: {product.ribbon}</p>}
+          {product.description && (
+            <div
+              dangerouslySetInnerHTML={{ __html: product.description }}
+              className="prose"
+            ></div>
+          )}
+          <ProductOptions
+            product={product}
+            selectedOptions={selectedOptions}
+            setSelectedOptions={setSelectedOptions}
+          />
         </div>
       </div>
     </div>
---
+++ src/app/products/[slug]/ProductOptions.tsx
@@ -0,0 +1,59 @@
+import { products } from "@wix/stores";
+
+interface ProductOptionsProps {
+  product: products.Product;
+  selectedOptions: Record<string, string>;
+  setSelectedOptions: (options: Record<string, string>) => void;
+}
+
+export default function ProductOptions({
+  product,
+  selectedOptions,
+  setSelectedOptions,
+}: ProductOptionsProps) {
+  return (
+    <div className="space-y-2.5">
+      {product.productOptions?.map((option) => (
+        <fieldset key={option.name} className="space-y-1.5">
+          <legend>
+            <span>{option.name}</span>
+          </legend>
+          <div className="5 flex flex-wrap items-center gap-1">
+            {option.choices?.map((choice) => (
+              <div key={choice.description}>
+                <input
+                  type="radio"
+                  id={choice.description}
+                  name={option.name}
+                  value={choice.description}
+                  onChange={() =>
+                    setSelectedOptions({
+                      ...selectedOptions,
+                      [option.name || ""]: choice.description || "",
+                    })
+                  }
+                  checked={
+                    selectedOptions[option.name || ""] === choice.description
+                  }
+                  className="peer hidden"
+                />
+                <label
+                  htmlFor={choice.description}
+                  className="flex min-w-14 cursor-pointer items-center justify-center gap-1.5 border p-2 peer-checked:border-black"
+                >
+                  {option.optionType === products.OptionType.color && (
+                    <span
+                      className="size-4 rounded-full border"
+                      style={{ backgroundColor: choice.value }}
+                    />
+                  )}
+                  <span>{choice.description}</span>
+                </label>
+              </div>
+            ))}
+          </div>
+        </fieldset>
+      ))}
+    </div>
+  );
+}
