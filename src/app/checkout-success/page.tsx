8922984793f218d65d6157edf5f94b1e1412c74b
Chris Ware <c.p.ware@hotmail.com>
2024-10-31 16:38:13 -0700

    Success page start

--- src/app/checkout-success/page.tsx
+++ src/app/checkout-success/page.tsx
@@ -1,4 +1,20 @@
-export default function Page() {
+import { getWixServerClient } from "@/lib/wix-client.server";
+import { getOrder } from "@/wix-api/orders";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Checkout Success",
+};
+
+interface PageProps {
+  searchParams: { orderId: string };
+}
+
+export default async function Page({ searchParams: { orderId } }: PageProps) {
+  const wixClient = getWixServerClient();
+
+  const [] = await Promise.all([getOrder(await wixClient, orderId)]);
+
   return (
     <main>
       <h1>Success!</h1>
---
+++ src/components/Order.tsx
@@ -0,0 +1,9 @@
+import { orders } from "@wix/ecom";
+
+interface OrderProps {
+  order: orders.Order;
+}
+
+export default function Order({ order }: OrderProps) {
+  return null;
+}
