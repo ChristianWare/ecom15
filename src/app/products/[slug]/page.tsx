b8d35ef8fa724b9499b31a5dbf00fe8440423592
Chris Ware <c.p.ware@hotmail.com>
2024-11-01 02:03:43 -0700

    related products done

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,8 +1,10 @@
-import { getProductBySlug } from "@/wix-api/products";
+import { getProductBySlug, getRelatedProducts } from "@/wix-api/products";
 import { notFound } from "next/navigation";
 import ProductDetails from "./ProductDetails";
 import { Metadata } from "next";
 import { getWixServerClient } from "@/lib/wix-client.server";
+import Product from "@/components/Product";
+import { Suspense } from "react";
 
 interface PageProps {
   params: { slug: string };
@@ -49,6 +51,34 @@ export default async function Page({ params }: PageProps) {
   return (
     <main className="py10 mx-auto max-w-7xl space-y-10 px-5">
       <ProductDetails product={product} />
+      <Suspense fallback={"Loading..."}>
+        <RelatedProducts productId={product._id} />
+      </Suspense>
+      <hr />
     </main>
+  );
+}
+
+interface RelatedProductsProps {
+  productId: string;
+}
+
+async function RelatedProducts({ productId }: RelatedProductsProps) {
+  const relatedProducts = await getRelatedProducts(
+    await getWixServerClient(),
+    productId,
+  );
+
+  if (!relatedProducts.length) return null;
+
+  return (
+    <div className="space-y-5">
+      <h2 className="text-2xl font-bold">Related Products</h2>
+      <div className="flex grid-cols-2 flex-col gap-5 sm:grid lg:grid-cols-4">
+        {relatedProducts.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
+      </div>
+    </div>
   );
 }
